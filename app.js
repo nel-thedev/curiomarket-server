98af8d3334a9c1d36f3fde1d30f8ac3ab4acec43
nel-thedev <nelelias@gmail.com>
2023-05-18 15:21:08 -0400

    Setup done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,7 +16,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('trust proxy', 1);
+app.enable('trust proxy');
+
+app.use(
+  cors({
+    origin: ['http://localhost:4000'],
+  })
+);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}" `
+    );
+  })
+  .catch((err) => console.log(err));
 
 module.exports = app;
